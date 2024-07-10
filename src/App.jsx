48576adae32700a951d67c1c4d9ed532abd00255
BacaV <nikolajevtic3405@gmail.com>
2024-07-10 20:31:09 +0200

    Main functionality and some css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const KEY = import.meta.env.VITE_GET_APP_ID;
 
+function App() {
+
+  const [ isLoading, setIsLoading ] = useState(true);
+  const [ city, setCity ] = useState("Belgrade");
+  const [ data, setData ] = useState({});
+
+
+  useEffect(() => {
+    getWeather(city, KEY);
+  }, []);
+
+  const getWeather = async (city, api) => {
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&units=metric`)
+        const json = await response.json();
+        setData(json);
+        console.log(data);
+    } catch (error){
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  function handleCityChange(e){
+    setCity(e.target.value);
+  }
+
+  function handleCity(){
+    getWeather(city, KEY)
+  }
+
+  
+
+  if(!isLoading){
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='background'>
+    </div>
+        <div className='weather-card'>
+          <h1 className='title'> Weather App. Made by Nikola J.</h1>
+          <div className='input-wrapper'>
+            <input type="text" id='cityInput' value={city} onChange={handleCityChange} />
+            <button onClick={handleCity}>Get Weather</button>
+          </div>
+
+          <h2>Todays weather in {data.name}</h2>
+          <div className='description'>
+            <img src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="" />
+            <p>{data.weather[0].description.toUpperCase()}</p>
+          </div>
+          <div className='data-wrapper'>
+            <div className='field-wrapper'>
+                <div className='data-field'>
+                    <p>Temperature: {data.main.temp}</p>
+                </div>
+                <div className='data-field'>
+                    <p>Max: {data.main.temp_max}</p>
+                </div>
+                <div className='data-field'>
+                    <p>Min: {data.main.temp_min}</p>
+                </div>
+                <div className='data-field'>
+                    <p>Feeling: {data.main.feels_like}</p>
+                </div>
+                
+              </div>
+              <div className='field-wrapper'>
+                <div className='data-field'>
+                    <p>Humidity: {data.main.humidity}</p>
+                </div>
+                <div className='data-field'>
+                    <p>Wind speed: {data.wind.speed}</p>
+                </div>
+                <div className='data-field'>
+                    <p>Lon: {data.coord.lon}</p>
+                </div>
+                <div className='data-field'>
+                    <p>Lat: {data.coord.lat}</p>
+                </div>
+              </div>
+          </div>
+          
+
+          
+        </div>
+    
     </>
-  )
+  );
+} else {
+  return null;
+}
 }
 
 export default App
